35d413032f395b5323dadeba745274bb5f6c738b
Jonathan Stroud <jstroud107@gmail.com>
2024-11-10 22:08:06 -0500

    First commit of server test

---
+++ README.md
@@ -0,0 +1,13 @@
+# Server (Still working on)
+
+Making a server from scratch using Tokio.
+
+## Things to Work on
+1. Setting up the HTTP stream correctly to use http request functions instead of the bytes
+2. Create an index.html page
+3. On the index.html page have the following informaton
+* Show IP Address
+* Show if the server has node js installed and what version
+* Show if the server has rust installed and what version
+* Show if python is installed and what version
+4. See how to deploy it on the network so other computers on the same network can see it
---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+use http_body_util::Full;
+use hyper::body::{ Frame, Bytes };
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+
+async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+}
+
+async fn connecttoserver(_: Request<hyper::body::Incoming>) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    Ok(Response::new(Full::new()
+)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let listener = TcpListener::bind(addr).await?;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let io = TokioIo::new(stream);
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                // `service_fn` converts our function in a `Service`
+                .serve_connection(io, service_fn(connecttoserver))
+                .await
+            {
+                eprintln!("Error serving connection: {:?}", err);
+            }
+        });
+    }
+}
