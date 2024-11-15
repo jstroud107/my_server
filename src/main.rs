482046f3e92824a15631cc55205c57c62b0d8ec5
Jonathan Stroud <jstroud107@gmail.com>
2024-11-15 00:27:45 -0500

    This is the latest update to my server

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,46 @@
-use std::fs;
-use std::net::SocketAddr;
-use std::convert::Infallible;
-use hyper::{body::Body, Request, Response, server, Method, StatusCode};
-use hyper::service::{service_fn};
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
+};
 
-async fn handle_request(req: Request<dyn Body>) -> Result<Response<dyn Body>, Infallible> {
-    match(req.method(), req.uri().path()) {
-        // Serve index.html for the root path
-        (&Method::GET, "/") => {
-            // Read the index.html file
-            match fs::read_to_string("index.html") {
-                Ok(contents) => Ok(Response::new(Body::from(contents))),
-                Err(_) => {
-                    let mut response = Response::new(Body::from("Internal Server Error"));
-                    *response.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
-                    Ok(response)
-                }
-            }
-        },
-        // Respond with 404 for any other paths
-        _ => {
-            let mut response = Response::new(Body::from("Not Found"));
-            *response.status_mut() = StatusCode::NOT_FOUND;
-            Ok(response)
-        }
+use my_server::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let pool = ThreadPool::new(4);
+
+    // add .take(#) to get a certain amount before shutting down
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
-#[tokio::main]
-async fn main() {
-    // Defining the address for the server to bind to
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
 
-    // Create the server and define a service
-    let make_svc = || async  {
-        Ok::<_, Infallible>(service_fn(handle_request))
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep.html HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "sleep.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
 
-    // Run the server
-    let server = Server::bind(&addr).serve(make_svc);
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
 
-    println!("Server running on http://{}", addr);
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    // Await server to start
-    if let Err(e) = server.await{
-        eprintln!("server error: {}", e);
-    }
-}
+    stream.write_all(response.as_bytes()).unwrap();
+    } 
